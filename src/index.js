9ecb817eb0e187e320ca21032211cb98372f72a8
veganbeef <veganbeef@protonmail.com>
2022-10-27 14:20:15 -0700

    updated eth rpc provider to infura

--- src/index.js
+++ src/index.js
@@ -6,11 +6,15 @@ import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 
 const activeChainId = ChainId.Mainnet;
 
+const chainRpcConfig = process.env.INFURA_API_KEY && process.env.INFURA_API_KEY.length > 0
+    ? {[ChainId.Mainnet]: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`}
+    : {};
+
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <ThirdwebProvider desiredChainId={activeChainId}>
+    <ThirdwebProvider desiredChainId={activeChainId} chainRpc={chainRpcConfig}>
       <App />
     </ThirdwebProvider>
   </React.StrictMode>
